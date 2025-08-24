1fcb0f6025b49138ed746eeba96da997473afd7a
devhwan0421 <devhwan0421@gmail.com>
2025-08-24 23:30:56 +0900

    Feat: LinkedList

--- LinkedList.h
+++ LinkedList.h
@@ -1,9 +1,32 @@
 #pragma once
 #include "IDataStructure.h"
+#include "Node.h"
 
 class LinkedList : public IDataStructure {
 public:
-	void* GetNode();
+	LinkedList();
+	~LinkedList();
+
+	Node* GetNode();
+
+	void PushFront(int value);
+	void PushBack(int value);
+	void Insert(int index, int value);
+
+	void PopFront();
+	void PopBack();
+	void Erase(int index);
+	void Clear();
+
+	Node* Search(int value);
+	int At(int index);
+
+	bool IsEmpty() const;
+	int GetSize() const;
+	void Print() const;
+
 private:
-	int* head;
+	int* m_head;
+	int* m_tail;
+	int m_size;
 };
--- Node.h
+++ Node.h
@@ -1,5 +1,7 @@
 #pragma once
 struct Node {
 	int m_data;
-	Node* m_link;
+	Node* m_next;
+
+	Node(int value) : m_data(value), m_next(nullptr) {}
 };
